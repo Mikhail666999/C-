bdc8c773d9f047ba888cb503f42a8c0297d7037c
Mikhail666999 <61342907+Mikhail666999@users.noreply.github.com>
2020-04-16 16:09:48 +0000

    Create 123.cpp

---
+++ 123.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string x;
+  x = "Привет или ты уже дома\n"
+  "привет или ты уже дома ";
+  cout <<x<< endl;
+  return 0;
+}
+    
