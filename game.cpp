154f4faead20f1fd6cb61892eb3a375cd1ae0637
Mikhail666999 <61342907+Mikhail666999@users.noreply.github.com>
2020-06-12 04:25:27 +0500

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "rpgRoom1.cpp"
+
+using namespace std;
+
+int main()
+{
+	
+	Room1();
+	
+	Room1Mon1();
+
+	Room1Mon1Shit();
+	
+	Room1Mon1Attack();
+	
+	Room1Mon1In_Flames();
+	
+	return 0;
+}
