3b256c5a8c43a65278667ceb0da4e63db28c0704
Mikhail666999 <61342907+Mikhail666999@users.noreply.github.com>
2020-06-13 00:14:39 +0500

    Create rpgFrist.cpp

---
+++ rpgFrist.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void Frist(){
+	cout << "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #" << endl;
+	cout << "#     Здравствуйте, вы зашли в подземелье. Сейчас это бета версия игры 'Rpg-Подземелье'             #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#     Вы будете писать команды и вы будете бить, защищатся, умирать возраждатся и т.д.              #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#     Сейчас здесь только одна комната и один монстр, (ну это ведь бета) ну команды                 #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#     будут снизу экрана, а все остальное поймете сами ха-ха-ха-ха-ха-ха-ха-ха-ха-ха-ха             #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                ЛОЛ                                                #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #" << endl;
+}
