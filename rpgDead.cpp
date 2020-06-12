948398ccb0256f0b972f420c2d5ff5536712e4b0
Mikhail666999 <61342907+Mikhail666999@users.noreply.github.com>
2020-06-13 00:15:14 +0500

    Create rpgDead.cpp

---
+++ rpgDead.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void Dead(){
+	cout << "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                              YOU DEAD                                             #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "#                                                                                                   #" << endl;
+	cout << "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #" << endl;
+}
