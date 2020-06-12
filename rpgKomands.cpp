c543b32879dd039103e9cb5b5e7c0470043dc516
Mikhail666999 <61342907+Mikhail666999@users.noreply.github.com>
2020-06-13 00:14:08 +0500

    Create rpgKomands.cpp

---
+++ rpgKomands.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void komanda1(){
+	cout << "/attack = нападение на монстра; /defend = закрыться щитом; /kill = убить себя;" << endl;
+ 
+}
+void komanda2(){
+
+}
